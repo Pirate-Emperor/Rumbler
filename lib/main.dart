a141f05a7e028359a52cd6b14226ef3c84be171b
Pirate-Emperor <piratekingrahul@gmail.com>
2022-10-26 00:53:51 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(primarySwatch: Colors.deepPurple
+          //brightness: Brightness.dark,
+          ),
+      //initialRoute: "/login",
+      routes: {
+        "/": (context) => const HomePage(),
+        "/login": (context) => const LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+          textScaleFactor: 2.0,
+        ),
+      ),
+    );
+  }
+}
